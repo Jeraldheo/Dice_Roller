1f03fb1c334feef36889472c0ad44c6859c54f60
Jeraldheo <jeyson.marin@unah.hn>
2022-10-09 17:21:29 -0600

    Finished adding backward compatibility for vector drawable.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,15 +3,22 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import com.example.diceroller.databinding.ActivityMainBinding
 import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity()
 {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val sides = listOf(R.drawable.dice_1, R.drawable.dice_2, R.drawable.dice_3,
+            R.drawable.dice_4, R.drawable.dice_5, R.drawable.dice_6)
+
+    private lateinit var dice: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,13 +27,14 @@ class MainActivity : AppCompatActivity()
         val rollButton: Button = binding.rollButton
 
         rollButton.setOnClickListener {
+                rollDice()
+        }
+        dice = binding.dice
+    }
 
-                val side  = Random.nextInt(1,7)
-                val dice: TextView = binding.dice
-                dice.text = side.toString()
-
-             }
-
-
+    private fun rollDice() {
+        val side  = Random.nextInt(0,6)
+        dice.setImageResource(sides[side])
+        Toast.makeText(this, side.toString(), Toast.LENGTH_SHORT).show()
     }
 }
